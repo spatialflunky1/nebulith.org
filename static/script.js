f295bc5a2a370888f02af730e4944487cbbe2867
Spatialflunky1 <mjandrus06@gmail.com>
2025-10-24 17:02:01 -0400

    Better design and functionality for the breadcrumb

--- static/script.js
+++ static/script.js
@@ -2,14 +2,28 @@ window.onload = function() {
     // Get breadcrumb element
     var breadcrumb_element = document.getElementById("breadcrumb").children[0];
 
-    // Compute current breadcrumb
-    var breadcrumb = "/Home";
+    // Get URL path (crumbs)
     var url = new URL(document.URL);
+    var paths = url.pathname.split("/")
     // Add to the breadcrumb excluding the origin slash on home
-    if (url.pathname.length != 1) {
-        breadcrumb += url.pathname;
-    }
+    if (paths.length > 1 && paths[1] != "") {
+        // Compute current breadcrumb
+        var breadcrumb = '<a class="crumb" href="/">Home</a>';
+        var pathbuild = document.location.origin;
+        for (const [index,path] of paths.entries()) {
+            if (path == "") continue;
+            else {
+                pathbuild += '/' + path;
+                if (index == paths.length-1) {
+                    breadcrumb += '/<a class="crumb" id="crumb_none">'+path+'</a>';
+                }
+                else {
+                    breadcrumb += '/<a class="crumb" href="'+pathbuild+'">'+path+'</a>';
+                }
+            }
+        }
 
-    // Set value of breadcrumb to breadcrumb element
-    breadcrumb_element.textContent = breadcrumb;
+        // Set value of breadcrumb to breadcrumb element
+        breadcrumb_element.innerHTML = breadcrumb;
+    }
 };
