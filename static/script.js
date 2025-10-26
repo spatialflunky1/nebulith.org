2613c6a83e760138ace730bc6bf6dc5167f05559
Spatialflunky1 <mjandrus06@gmail.com>
2025-10-26 12:52:49 -0400

    Fixed footer position on long pages

--- static/script.js
+++ static/script.js
@@ -1,4 +1,34 @@
+function fix_footer_position() {
+    //
+    // Fix footer position
+    //
+
+    // Get innner body div
+    var body_div = document.getElementById("body");
+
+    // Check if content extends into footer
+    //     True when body div height > (document height - navibar height - footer height)
+    // If so, disable absolute positioning of footer and let the browser push it to the bottom
+    var navibar = document.getElementById("navibar");
+    var footer = document.getElementById("footer");
+    if (body_div.offsetHeight > (window.innerHeight - navibar.offsetHeight - footer.offsetHeight)) {
+        footer.style.position = "static";
+        // Set the margin to be acceptable with this positioning
+        body_div.style.marginBottom = "16px";
+    }
+    // This function goes both ways so it should set it back otherwise
+    else {
+        footer.style.position = "absolute";
+        // Set the margin to be acceptable with this positioning
+        body_div.style.marginBottom = "141px";
+    }
+}
+
 window.onload = function() {
+    //
+    // Set breadcrumb
+    //
+
     // Get breadcrumb element
     var breadcrumb_element = document.getElementById("breadcrumb").children[0];
 
@@ -26,4 +56,9 @@ window.onload = function() {
         // Set value of breadcrumb to breadcrumb element
         breadcrumb_element.innerHTML = breadcrumb;
     }
+    fix_footer_position();
+};
+
+window.onresize = function() {
+    fix_footer_position();
 };
