0bd76d4fb584881bd34644716b4796924c664d31
Spatialflunky1 <mjandrus06@gmail.com>
2025-10-29 02:13:57 +0000

    Changed gunicorn's binding address

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #gunicorn -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker --bind localhost -m 007 main:app
-gunicorn -k eventlet --bind localhost -m 007 main:app
+gunicorn -k eventlet --bind 0.0.0.0:8000 -m 007 main:app
